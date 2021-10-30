6b37910f3c2eeb9d936d0d90b4af6031ef6d597f
Iannisbib <88343429+Iannisbib@users.noreply.github.com>
2021-10-30 17:24:25 -0300

    cambioiannis

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -27,6 +27,12 @@ const routes: Routes = [
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   },
+  
+  {
+    path: 'search',
+    loadChildren: () => import('./search/search.module').then( m => m.SearchPageModule)
+  },
+
 
 ];
 
